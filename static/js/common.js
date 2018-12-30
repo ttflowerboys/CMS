ba80c7eb5dcb85a9356f7aa0703daf80f154679f
leon <threetowns@163.com>
2018-12-31 01:16:52 +0800

    Add: login feature.

--- static/js/common.js
+++ static/js/common.js
@@ -1,3 +1,85 @@
+// 检查 email 格式
+function IsEmail(strg) {
+    //var patrn = new RegExp('^[0-9a-zA-Z_\-\.]+@[0-9a-zA-Z_\-]+(\.[0-9a-zA-Z_\-]+)*$'); 
+    var patrn = new RegExp('^\\w+((-\\w+)|(\\.\\w+))*\\@[A-Za-z0-9]+((\\.|-)[A-Za-z0-9]+)*\\.[A-Za-z0-9]+$');
+    if (!patrn.test(strg))
+        return false;
+    return true;
+}
+
+// 验证电话
+function IsTel(strg) {
+    var patrn = new RegExp('^(([0\+]\d{2,3}-)?(0\d{2,3})-)(\d{7,8})(-(\d{3,}))?$');
+    if (!patrn.test(strg))
+        return false;
+    return true;
+}
+
+// 验证手机
+function IsMobile(strg) {
+    //var patrn = new RegExp("^0?(13[0-9]|14[0-9]|15[0-9]|17[0-9]|18[0-9]|19[0-9])[0-9]{8}$");
+    var patrn = new RegExp("^1[\\d]{10}$");
+    //var patrn = new RegExp('^(13|15|18)[0-9]{9}$');
+    if (!patrn.test(strg))
+        return false;
+    return true;
+}
+
+// 验证邮编
+function IsZip(strg) {
+    var patrn = new RegExp('^\\d{6}$');
+    if (!patrn.test(strg))
+        return false;
+    return true;
+}
+
+// 验证整数
+function IsInt(strg) {
+    var patrn = new RegExp('^\\d{1,10}$');
+    if (!patrn.test(strg))
+        return false;
+    return true;
+}
+
+//验证浮点数
+function IsFloat(strg){//?([0-9]{0,9})?(.([0-9]{1,2}))
+    var patrn = new RegExp('^[1-9]{0,1}[0-9]{0,9}(\.[0-9]{1,2})?$');
+    if (!patrn.test(strg))
+        return false;
+    return true;
+}
+
+//验证IP
+function IsIP(strg){
+    var patrn = new RegExp('^([0-9]{1,3}\.{1}){3}[0-9]{1,3}$');
+    if (!patrn.test(strg))
+        return false;
+    return true;
+}
+
+//验证空(去除空格和回车)
+function IsEmpty(str){
+	if(!str) return false;
+	var resultStr = str.replace(/\ +/g, ""); //去掉空格
+		if(resultStr.length == 0) return false;
+        var resultStr2 = resultStr.replace(/[\r\n]/g, ""); //去掉回车换行
+        if(resultStr2.length == 0) return false;
+        return true;
+}
+
+//验证密码
+function IsPass(strg){
+	/*//var patrn = new RegExp("^[a-zA-Z0-9\u4e00-\u9fa5]{6,18}$");
+	if (!patrn.test(strg))
+        return false;
+    return true;*/
+    if(strg.length < 6 || strg.length > 18) return false;
+    return true;
+}
+
+
+
+
 $(function () {
     // ajax loading
     function show_loading_body() {
