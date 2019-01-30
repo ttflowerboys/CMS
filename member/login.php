997b44ed7d7b89f4771cd55d2f5b2a99a93bf992
leon <threetowns@163.com>
2019-01-31 00:33:57 +0800

    Mod: member login.

--- member/login.php
+++ member/login.php
@@ -12,4 +12,4 @@ if($cfg_ml->IsLogin())
     ShowMsg('你已经登陆系统，无需重新注册！', 'index.php');
     exit();
 }
-require_once(dirname(__FILE__)."/templets/login.htm");
+require_once(dirname(__FILE__)."/templets/index-login.htm");
